04ed6127513acca2e17119060eb049090d429335
GabeALopez <109094200+GabeALopez@users.noreply.github.com>
2023-09-21 16:07:26 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lopez_Gabriel_Pass_Receive_Between_Activities"
+include(":app")
