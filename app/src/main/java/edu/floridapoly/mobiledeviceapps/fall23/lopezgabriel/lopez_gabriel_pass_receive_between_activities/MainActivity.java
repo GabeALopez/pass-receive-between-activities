efdb467c1b91639890f810348b7a1d78481c5b30
GabeALopez <109094200+GabeALopez@users.noreply.github.com>
2023-09-23 16:46:23 -0400

    Started adding code for intents

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/lopezgabriel/lopez_gabriel_pass_receive_between_activities/MainActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/lopezgabriel/lopez_gabriel_pass_receive_between_activities/MainActivity.java
@@ -2,7 +2,12 @@ package edu.floridapoly.mobiledeviceapps.fall23.lopezgabriel.lopez_gabriel_pass_
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCourseActivity = (Button) (R.id.btnCourseActivity);
+        Button btnSongActivity = (Button) (R.id.btnSongActivity);
+        EditText edtCourse = (EditText) findViewById(R.id.edtFavoriteCourse);
+        EditText edtSong = (EditText) findViewById(R.id.edtFavoriteSong);
+        TextView txtMyName = (TextView) findViewById(R.id.txtMyName);
+
+        btnCourseActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                string strCourse = edtCourse.getText().toString();
+                string strNameCourse = txtMyName.getText().toString();
+
+                Intent intentCourse = new Intent(getApplicationContext(), favoriteCourse.class);
+                intentCourse.putExtra("message_key1", strCourse);
+
+                Intent intentNameCourse = new Intent(getApplicationContext(), favoriteSong.class);
+                intentNameCourse.putExtra("message_key2", strNameCourse);
+            }
+        });
+
+        btnSongActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                string strSong = edtSong.getText().toString();
+                string strNameSong = txtMyName.getText().toString();
+
+                Intent intentSong = new Intent(getApplicationContext(), favoriteSong.class);
+                intentSong.putExtra("message_key1", strSong);
+
+                Intent intentNameSong = new Intent(getApplicationContext(), favoriteSong.class);
+                intentNameSong.putExtra("message_key2", strNameSong);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/lopezgabriel/lopez_gabriel_pass_receive_between_activities/favoriteCourse.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/lopezgabriel/lopez_gabriel_pass_receive_between_activities/favoriteCourse.java
@@ -3,12 +3,21 @@ package edu.floridapoly.mobiledeviceapps.fall23.lopezgabriel.lopez_gabriel_pass_
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class aboutMe extends AppCompatActivity {
+public class favoriteCourse extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorite_course);
+
+        TextView receivedInfoCourse = (TextView) (R.id.txtFavoriteCourse);
+        Intent intentCourse = getIntent();
+
+        string strCourse = intentCourse.getStringExtra("message_key1");
+
+
+
     }
 }
